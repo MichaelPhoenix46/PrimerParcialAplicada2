39caf06f31a5dd108dee31c22191da06ea327c06
Jose Michael castillo <39072097+MichaelPhoenix46@users.noreply.github.com>
2019-02-06 14:38:14 -0400

    wut

---
+++ DAL/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Entities;
+using System.Data.Entity;
+
+namespace DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Cuenta> Cuenta { get; set; }
+        public DbSet<Deposito> Deposito { get; set; }
+
+        public Contexto() : base("ConStr")
+        {
+        }
+    }
+}
---
+++ DAL/Scripts/Script.sql
@@ -0,0 +1,1 @@
+create database PrimerParcial2
