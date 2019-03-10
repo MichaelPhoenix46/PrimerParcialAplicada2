e387ea1f28549231b523a65ec776743101d9d46d
Jose Michael castillo <39072097+MichaelPhoenix46@users.noreply.github.com>
2019-03-10 05:01:51 -0400

    agregando entidades prestamo y detalle. agregando prestamo a dal

---
+++ Entities/DetallePrestamo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    [Serializable]
+    public class DetallePrestamo
+    {
+        [Key]
+        public int DetalleId { get; set; }
+        public DateTime Fecha { get; set; }
+        public int PrestamoId { get; set; }
+        public int Cuota { get; set; }
+        public int CuentaId { get; set; }
+        public decimal InteresMensual { get; set; }
+        public decimal CapitalMensual { get; set; }
+        public decimal Balance { get; set; }
+        [ForeignKey("CuentaId")]
+        public virtual Cuenta Cuenta { get; set; }
+
+
+        public DetallePrestamo()
+        {
+            DetalleId = 0;
+            Fecha = DateTime.Now;
+            PrestamoId = 0;
+            Cuota = 0;
+            CuentaId = 0;
+            InteresMensual = 0;
+            CapitalMensual = 0;
+            Balance = 0;
+        }
+
+        public DetallePrestamo(int id, DateTime fecha, int prestamoId, int cuota, int cuentaId, decimal interesMensual, decimal capitalMensual, decimal balance)
+        {
+            DetalleId = id;
+            Fecha = fecha;
+            PrestamoId = prestamoId;
+            Cuota = cuota;
+            CuentaId = cuentaId;
+            InteresMensual = interesMensual;
+            CapitalMensual = capitalMensual;
+            Balance = balance;
+        }
+    }
+}
---
+++ Entities/Prestamo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    [Serializable]
+    public class Prestamo
+    {
+        [Key]
+        public int PrestamoId { get; set; }
+        public int CuentaId { get; set; }
+        public int Capital { get; set; }
+        public int Meses { get; set; }
+        public int InteresAnual { get; set; }
+        public decimal Total { get; set; }
+        public virtual List<DetallePrestamo> Detalle { get; set; }
+        public DateTime Fecha { get; set; }
+
+
+        public Prestamo()
+        {
+            Detalle = new List<DetallePrestamo>();
+        }
+
+        public void AgregarDetalle(int Id, DateTime Fecha, int PrestamoId, int Cuota, int CuentaId, decimal InteresMensual, int CapitalMensual, decimal Balance)
+        {
+            Detalle.Add(new DetallePrestamo(Id, Fecha, PrestamoId, Cuota, CuentaId, InteresMensual, CapitalMensual, Balance));
+        }
+
+    }
+}
